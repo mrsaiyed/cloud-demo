721e98a04888e9a84eef0d472ca402d4ede57713
mrsaiyed <mr.saiyed14@gmail.com>
2025-04-27 20:08:36 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+# Connect to the Neon database
+connection = psycopg2.connect(
+    host=os.environ['DB_HOST'],
+    database=os.environ['DB_NAME'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASSWORD'],
+    sslmode='require'
+)
+
+@app.route('/')
+def hello():
+    cursor = connection.cursor()
+    cursor.execute('SELECT message FROM hello LIMIT 1;')
+    result = cursor.fetchone()
+    cursor.close()
+    return result[0] if result else 'No message found in database.'
+
+if __name__ == '__main__':
+    app.run()
